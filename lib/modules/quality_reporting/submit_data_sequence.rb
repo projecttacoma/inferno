57bb6641965047a16510b9531c695e929cb1fb25
lmd59 <laurend@mitre.org>
2021-04-27 10:22:19 -0400

    single resource test

--- lib/modules/quality_reporting/submit_data_sequence.rb
+++ lib/modules/quality_reporting/submit_data_sequence.rb
@@ -52,6 +52,41 @@ module Inferno
           assert(search_bundle.total.positive?, "Search for a #{r.resourceType} with identifier #{identifier} returned no results")
         end
       end
+
+      test 'Submit Data single resource submission' do
+        metadata do
+          id '02'
+          link 'https://www.hl7.org/fhir/measure-operation-submit-data.html'
+          description 'Submit resources relevant to a measure, and then verify they persist on the server.'
+        end
+
+        assert(!@instance.measure_to_test.nil?, 'No measure selected. You must run the Prerequisite sequences prior to running Reporting Actions sequences.')
+
+        @client.additional_headers = { 'x-api-key': @instance.api_key, 'Authorization': @instance.auth_header } if @instance.api_key && @instance.auth_header
+
+        resources = [get_data_requirements_resources(@instance.data_requirements_queries).sample]
+        measure_report = create_measure_report(@instance.measure_to_test, '2019', '2019')
+
+        # Submit the data
+        submit_data_response = submit_data(@instance.measure_to_test, resources, measure_report)
+        assert_response_ok(submit_data_response)
+
+        resources.push(measure_report)
+
+        # GET and assert presence of all submitted resources
+        resources.each do |r|
+          identifier = r.identifier&.first&.value
+          assert !identifier.nil?
+
+          # Search for resource by identifier
+          search_response = @client.search(r.class, search: { parameters: { identifier: identifier } })
+          assert_response_ok search_response
+          search_bundle = search_response.resource
+
+          # Expect a non-empty searchset Bundle
+          assert(search_bundle.total.positive?, "Search for a #{r.resourceType} with identifier #{identifier} returned no results")
+        end
+      end
     end
   end
 end
