a623da70e9f8825ce78dcc3c6866c48abc75adde
Matthew Gramigna <mgramigna@mitre.org>
2021-06-29 11:32:03 -0400

    Code from Connectathon (#33)
    * Update submit data to fail safely on various edgecases and update parameter casing
    
    * Include more fallback logic and disable measure-eval sequence
    
    * Updates to testing flow
    
    * Use pipe-delimited instance var for measure identifier/version
    * Use proper measure reference for submit data measureReport
    * Add search calls for measure by identifier and version when needed
    * Add TODOs for future tasking
    
    * Submit default set of resources when data requirements fails
    
    * Add automated updateCodeSystems call
    
    * Remove stray binding.pry
    
    * Remove query workaround and extra spacing

--- lib/app/utils/measure_operations.rb
+++ lib/app/utils/measure_operations.rb
@@ -27,15 +27,13 @@ module Inferno
       @client.get "#{endpoint}#{params_string}", @client.fhir_headers(format: FHIR::Formats::ResourceFormat::RESOURCE_JSON)
     end
 
-    def create_measure_report(measure_id, period_start, period_end)
+    def create_measure_report(measure_url, period_start, period_end)
       FHIR::MeasureReport.new.from_hash(
         type: 'data-collection',
         identifier: [{
           value: SecureRandom.uuid
         }],
-        measure: {
-          reference: "Measure/#{measure_id}"
-        },
+        measure: measure_url,
         period: {
           start: period_start,
           end: period_end
@@ -46,14 +44,13 @@ module Inferno
 
     def submit_data(measure_id, patient_resources, measure_report)
       parameters = FHIR::Parameters.new
-      # TODO: this should ideally be named "measureReport" from spec: https://www.hl7.org/fhir/measure-operation-submit-data.html
-      measure_report_param = FHIR::Parameters::Parameter.new(name: 'measurereport')
+      measure_report_param = FHIR::Parameters::Parameter.new(name: 'measureReport')
       measure_report_param.resource = measure_report
       parameters.parameter.push(measure_report_param)
 
       patient_resources.each do |r|
         # create unique identifier if not present on resource
-        if !r.identifier&.first&.value
+        unless r.identifier&.first&.value
           i = FHIR::Identifier.new
           i.value = SecureRandom.uuid
           r.identifier = [i]
@@ -80,6 +77,20 @@ module Inferno
 
     def get_measure_resources_by_name(measure_name)
       @client.get "Measure?name=#{measure_name}", @client.fhir_headers(format: FHIR::Formats::ResourceFormat::RESOURCE_JSON)
+    end
+
+    def get_measure_from_test_server(measure_identifier, measure_version)
+      get_measure_by_identifier_and_version(@client, measure_identifier, measure_version)
+    end
+
+    def get_measure_from_embedded_server(measure_identifier, measure_version)
+      get_measure_by_identifier_and_version(cqf_ruler_client, measure_identifier, measure_version)
+    end
+
+    def get_measure_by_identifier_and_version(client, measure_identifier, measure_version)
+      resp = client.search(FHIR::Measure, search: { parameters: { identifier: measure_identifier, version: measure_version } })
+      bundle = FHIR::Bundle.new JSON.parse(resp.body)
+      bundle.entry.first&.resource
     end
 
     def async_submit_data(params_resource)
@@ -149,12 +160,11 @@ module Inferno
     end
 
     def get_library_resource(library_id)
-      libraries_endpoint = Inferno::CQF_RULER + 'Library'
       library_request = cqf_ruler_client.search(FHIR::Library, search: { parameters: { url: library_id } })
       raise StandardError, "Could not retrieve library #{library_id} from CQF Ruler." if library_request.code != 200
 
       lib = nil
-      if library_request.resource.total == 0
+      if library_request.resource.zero?
         library_request = cqf_ruler_client.read(FHIR::Library, library_id)
         raise StandardError, "Could not retrieve library #{library_id} from CQF Ruler." if library_request.code != 200
 
@@ -165,6 +175,7 @@ module Inferno
       end
 
       raise StandardError, "Error obtaining library #{library_id} from response body" if lib.nil?
+
       lib
     end
 
@@ -183,7 +194,7 @@ module Inferno
       refs = library.relatedArtifact.select { |ref| ref.type == 'depends-on' }
       refs.lazy
         .select { |ref| ref.resource.include? 'Library/' }
-        .map{ |ref| ref.resource.split('|').first }
+        .map { |ref| ref.resource.split('|').first }
         .to_a
     end
 
@@ -195,45 +206,76 @@ module Inferno
         .to_a
     end
 
+    def update_code_systems
+      # Expand valuesets into codesystem resources to support code:in queries
+      response = cqf_ruler_client.client.get(Inferno::CQF_RULER + '/$updateCodeSystems')
+      raise StandardError, 'Error updating codesystems' if response.code != 200
+    end
+
     def get_data_requirements_queries(data_requirement)
       # hashes with { endpoint => FHIR Type, params => { queries } }
-      data_requirement
-        .select { |dr| dr&.codeFilter&.first&.code&.first || dr&.codeFilter&.first&.valueSet }
+      queries = data_requirement
+        .select { |dr| dr&.type }
         .map do |dr|
           q = { 'endpoint' => dr.type, 'params' => {} }
 
           # prefer specific code filter first before valueSet
-          if dr.codeFilter.first.code&.first
+          if dr.codeFilter&.first&.code&.first
             q['params'][dr.codeFilter.first.path.to_s] = dr.codeFilter.first.code.first.code
-          elsif dr.codeFilter.first.valueSet
+          elsif dr.codeFilter&.first&.valueSet
             q['params']["#{dr.codeFilter.first.path}:in"] = dr.codeFilter.first.valueSet
           end
 
           q
         end
+
+      # TODO: We should be smartly querying for patients based on what the resources reference?
+      queries.unshift('endpoint' => 'Patient', 'params' => {})
+      queries
     end
 
     def get_data_requirements_resources(queries)
+      # If data requirements failed, default to a canned list of resources to test $submit-data
+      if queries.empty?
+        default_queries = [
+          { 'endpoint' => 'Patient', 'params' => {} },
+          { 'endpoint' => 'Encounter', 'params' => {} },
+          { 'endpoint' => 'Condition', 'params' => {} },
+          { 'endpoint' => 'Procedure', 'params' => {} },
+          { 'endpoint' => 'Observation', 'params' => {} }
+        ]
+        @instance.update(data_requirements_queries: default_queries)
+        @instance.save!
+
+        queries = @instance.data_requirements_queries
+      end
+
       queries
         .map do |q|
-          endpoint = Inferno::CQF_RULER + q.endpoint
-          params_string = q.params.empty? ? '' : "?#{q.params.to_query}"
-          
-          begin
-            # TODO: run query through unlogged rest client
-            response = cqf_ruler_client.client.get("#{endpoint}#{params_string}")
-            code = response.code
-          rescue RestClient::NotFound => e
-            code = 404
-          end
-          
-          # TODO: get rid of this backup call (replace with assertion) once we have a working fhir server source for valueset search
-          response = cqf_ruler_client.client.get("#{endpoint}") if code != 200
+        endpoint = Inferno::CQF_RULER + q.endpoint
+        params_string = q.params.empty? ? '' : "?#{q.params.to_query}"
 
+        begin
+          # TODO: run query through unlogged rest client
+          response = cqf_ruler_client.client.get("#{endpoint}#{params_string}")
+          code = response.code
+        rescue RestClient::PreconditionFailed
+          # TODO: Happens with niche codesystem error on HAPI systems. Fix need for this
+          # Note: calling the $updateCodeSystems endpoint on cqf-ruler should resolve this
+          code = 412
+        rescue RestClient::NotFound
+          code = 404
+        end
+
+        # Return all resources in the response bundle if queries are met
+        if code == 200
           bundle = FHIR::Bundle.new JSON.parse(response.body)
           bundle.entry.map(&:resource)
+        else
+          []
         end
-      .flatten.uniq{|r| r.id}
+      end
+        .flatten.uniq(&:id)
     end
   end
 end
--- lib/modules/quality_reporting/data_requirements_sequence.rb
+++ lib/modules/quality_reporting/data_requirements_sequence.rb
@@ -26,24 +26,27 @@ module Inferno
         end
 
         assert(!@instance.measure_to_test.nil?, 'No measure selected. You must run the Prerequisite sequences prior to running this sequence.')
-        measure_id = @instance.measure_to_test
+
+        measure_identifier, measure_version = @instance.measure_to_test.split('|')
+        measure_resource_embedded = get_measure_from_embedded_server(measure_identifier, measure_version)
+        measure_resource_system = get_measure_from_test_server(measure_identifier, measure_version)
 
         # Get data requirements from cqf-ruler
-        expected_results_library = get_data_requirements(measure_id, PARAMS.compact)
+        expected_results_library = get_data_requirements(measure_resource_embedded.id, PARAMS.compact)
         expected_dr = expected_results_library.dataRequirement
 
         # Get data requirements from client
-        data_requirements_response = data_requirements(measure_id, PARAMS.compact)
+        data_requirements_response = data_requirements(measure_resource_system.id, PARAMS.compact)
         assert_response_ok data_requirements_response
 
         # Load response body into a FHIR Library class, expected to contain dataRequirement array
         data_library = FHIR.from_contents(data_requirements_response.body)
         actual_dr = data_library&.dataRequirement
-        assert(!actual_dr.nil?, "Client provided no data requirements for measure #{measure_id}")
+        assert(!actual_dr.nil?, "Client provided no data requirements for measure #{@instance.measure_to_test}")
 
         # Compare data requirements to expected
-        assert((expected_dr - actual_dr).blank?, "Client data-requirements is missing expected data requirements for measure #{measure_id}")
-        assert((actual_dr - expected_dr).blank?, "Client data-requirements contains unexpected data requirements for measure #{measure_id}")
+        assert((expected_dr - actual_dr).blank?, "Client data-requirements is missing expected data requirements for measure #{@instance.measure_to_test}")
+        assert((actual_dr - expected_dr).blank?, "Client data-requirements contains unexpected data requirements for measure #{@instance.measure_to_test}")
 
         # store data requirements queries for future sequence use
         queries = get_data_requirements_queries(actual_dr)
